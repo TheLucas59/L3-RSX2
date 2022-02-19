8fd7b3b44a634290b820bf40a0ffed24853af4ff
Plé Lucas <lucas.ple.etu@univ-lille.fr>
2022-02-19 15:19:47 +0100

    requete fonctionnel et réponse correcte

--- tp1/DNS.c
+++ tp1/DNS.c
@@ -8,24 +8,25 @@
 #include <unistd.h>
 
 #define PORT 53
-#define DNS_ADDR "127.0.0.53"
+#define DNS_ADDR "193.49.225.15"
 #define BUFFER_SIZE 1024
+#define REQUEST_LENGTH 29
 
 void sendDNSRequest(int socket_dns, struct sockaddr_in dest) {
-    char request[] = {
-        0x12, 0xcc, 0x00, 0x00,    //entête
-        0x80, 0x01, 0x00, 0x00,
+    const char request[REQUEST_LENGTH] = {
+        0x08, 0xbb, 0x01, 0x00,    //entête
+        0x00, 0x01, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00,
-        0x03,  0x77,  0x77,  0x77, //question    
-        0x04,  0x6c,  0x69,  0x66, //QNAME : 3"www" 4"lifl" 2"fr" 0 
-        0x6c,  0x02,  0x66,  0x72,
+        0x03, 0x77, 0x77, 0x77, //question    
+        0x04, 0x6c, 0x69, 0x66, //QNAME : 3"www" 4"lifl" 2"fr" 0 
+        0x6c, 0x02, 0x66, 0x72,
         0x00,
         0x00, 0x01,                //Q-Type
         0x00, 0x01                 //Q-Class
     };
-    if(sendto(socket_dns, request, strlen(request), 0, (struct sockaddr*) &dest, sizeof(dest)) == -1) {
+    if( sendto(socket_dns, request, REQUEST_LENGTH, 0, (struct sockaddr*) &dest, sizeof(dest)) == -1) {
         perror("sendto");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -38,7 +39,7 @@ int main() {
 
     struct sockaddr_in dest;
     dest.sin_family = AF_INET;
-    dest.sin_port = PORT;
+    dest.sin_port = htons(PORT);
 
     in_addr_t adresseDest = inet_addr(DNS_ADDR);
     if(adresseDest == INADDR_NONE) {
@@ -49,6 +50,7 @@ int main() {
     dest.sin_addr.s_addr = adresseDest;
 
     sendDNSRequest(socket_dns, dest);
+    printf("Message envoyé.\n");
 
     struct sockaddr_in addrRecv;
     socklen_t addrRecvlen = sizeof(struct sockaddr_in);
@@ -61,10 +63,34 @@ int main() {
       exit(EXIT_FAILURE);
     }
 
-    printf("%s\n", buffer);
+    printf(" - port  distant  : %hu\n", ntohs(addrRecv.sin_port));
+    printf(" - @IPv4 distante : %s\n", inet_ntoa(addrRecv.sin_addr));
+    printf("longueur du message recu : %u\n", received);
 
-    printf("Message envoyé.\n");
+    //printf("%s\n", buffer);
+
+    printf("Message reçu.\n");
     close(socket_dns);
+
+    for (int i = 0; i < received; i++) {
+
+        fprintf(stdout," %.2X", buffer[i] & 0xff);
+
+        if (((i+1)%16 == 0) || (i+1 == received)) {
+
+            /* ceci pour afficher les caracteres ascii apres l'hexa */
+            /* >>> */
+            for (int j = i+1 ; j < ((i+16) & ~15); j++) {
+            fprintf(stdout,"   ");
+            }
+            fprintf(stdout,"\t");
+            for (int j = i & ~15; j <= i; j++)
+            fprintf(stdout,"%c",buffer[j] > 31 && buffer[j] < 128 ? (char)buffer[j] : '.');
+            /* <<< */
+            fprintf(stdout,"\n");
+        }
+    }
+
 
     return 0;
 }
